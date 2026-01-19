96a515564495acc81739c3cdb944c94fa3125a74
oyb <oyb@oybs-mbp.home>
2026-01-19 17:08:16 +0100

    Ready for App Store

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,21 +1,13 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.1611156aef0444539ec63614710539dd',
-  appName: 'blatt',
+  appId: 'com.arzmadeit.blatt',
+  appName: 'Blatt',
   webDir: 'dist',
   server: {
-    url: 'https://1611156a-ef04-4453-9ec6-3614710539dd.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
-  ios: {
-    contentInset: 'automatic',
-    preferredContentMode: 'mobile',
-    backgroundColor: '#0d1117',
-  },
-  android: {
-    backgroundColor: '#0d1117',
-  },
+    url: 'https://blatt.lovable.app',
+    cleartext: false
+  }
 };
 
 export default config;
